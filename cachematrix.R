ee16cc1a2411b66a646367b718dabd327a37a4ad
Vivek Tomar <tomarforever86@gmail.com>
2020-04-21 20:47:00 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,26 @@
-makeCacheMatrix <- function(x = matrix()) {
-  ## Initialize the inverse property
-  i <- NULL
-  
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) 
+{
+  ## get value of matrix
+  inv <- NULL
+
   ## To set the matrix
-    set <- function( matrix ) {
-    m <<- matrix
-    i <<- NULL
+    set <- function(y) {
+    x <<- y
+    inv <<- NULL
   }
 }
 
-## To get the matrix
-get <- function() {
-  ## Return the matrix
-  m
-}
+## To get the matrix and Return the matrix
+get <- function() { x }
+
 
 ## To set the inverse of the matrix
-setInverse <- function(inverse) {
-  i <<- inverse
-}
+setInverse <- function(inverse) { inv <<- inverse }
 
 ## To get the inverse of the matrix
-getInverse <- function() {
-  ## Return the inverse property
-  i
-}
+getInverse <- function() { inv }
 
 ## Return a list of the methods
 list(set = set, get = get,
@@ -32,26 +28,30 @@ list(set = set, get = get,
      getInverse = getInverse)
 }
 
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
   ## Return a matrix that is the inverse of 'x'
-  m <- x$getInverse()
+  inv <- x$getInverse()
   
   ## Just return the inverse if its already set
-  if( !is.null(m) ) {
+  if( !is.null(inv) ) {
     message("getting cached data")
-    return(m)
+    return(inv)
   }
   
   ## Get the matrix from our object
   data <- x$get()
   
   ## Calculate the inverse using matrix multiplication
-  m <- solve(data) %*% data
+  inv <- solve(data, ...) 
   
   ## Set the inverse to the object
-  x$setInverse(m)
+  x$setInverse(inv)
   
   ## Return the matrix
-  m
+  inv
 }
